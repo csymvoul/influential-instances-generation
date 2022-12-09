c26739074fd98ee46b7ff60c222b1b46210ff1d9
Chrysostomos Symvoulidis <simvoul@unipi.gr>
2022-12-09 15:17:01 +0200

    args updated

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import sys
 import warnings
 from src.data import Data
-from src.models_enum import ModelType, ModelName
+from src.enums import ModelType, ModelName, Datasets
 from src.model import Model
 
-print('Number of arguments: {}'.format(len(sys.argv)))
+import argparse, sys
+
+parser=argparse.ArgumentParser()
+
+parser.add_argument("--data", help="The dataset used for training of the model.")
+parser.add_argument("--model", help="The model which will be used for training.")
+
+args=parser.parse_args()
 
 try: 
-    dataset_file_name = sys.argv[1]
+    dataset_file_name = Datasets(args.data)
     data = Data(dataset_file_name=dataset_file_name)
     try: 
-        model_name = ModelName(sys.argv[2])
+        model_name = ModelName(args.model)
         model = Model(model=model_name, data=data)
     except:
         warnings.warn('No model specified. Using the default model (LogisticRegression)')
@@ -18,12 +25,14 @@ try:
 except:
     warnings.warn('No dataset specified. Using the default dataset (breast_cancer)')
     try: 
-        model_name = ModelName(sys.argv[2])
+        model_name = ModelName(args.model)
         model = Model(model=model_name)
     except:
-        warnings.warn('No model type specified. Using the default model (LogisticRegression)')
+        warnings.warn('No model type specified. Using the default model (LogisticRegression)', category=DeprecationWarning)
         model = Model()
     
 print(model.get_model().value)
 print(model.get_type().value)
-print(model.get_data().visualize_data())
+print(model.get_data().get_dataset_file_name())
+model.get_data().set_dataset(dataset_file_name=Datasets.CervicalCancer)
+print("Now the dataset is: ", model.get_data().get_dataset_file_name())
